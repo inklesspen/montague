942cb6b429ffb0cea463eba1ec52d9f4a88d154e
Jon Rosebaugh <jon@inklesspen.com>
2014-11-14 21:49:01 -0500

    Correct import for converters. (Whoops!)

--- src/montague/compat/config.py
+++ src/montague/compat/config.py
@@ -154,7 +154,7 @@ def make_prefix_middleware(
         app, global_conf, prefix='/',
         translate_forwarded_server=True,
         force_port=None, scheme=None):
-    from paste.deploy.converters import asbool
+    from .converters import asbool
     translate_forwarded_server = asbool(translate_forwarded_server)
     return PrefixMiddleware(
         app, prefix=prefix,
